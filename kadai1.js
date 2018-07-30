b566209245f8b652c32d6214ca003f9a139dfbfb
zm16290 <zm16290@josai.ac.jp>
2018-07-30 00:01:42 -0400

    up

---
+++ kadai1.js
@@ -0,0 +1,11 @@
+function ganmen1(){
+  document.getElementById('kao').innerHTML='<img src="./image/kusoganmen2.png">'
+}
+
+function ganmen2(){
+  document.getElementById('kao').innerHTML='<img src="./image/kusoganmen3.png">'
+}
+
+function No_image(){
+  document.getElementById('kao').innerHTML='<img src="./image/noimage.png" width="200" height="100">'
+}
